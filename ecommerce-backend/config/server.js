8aa3de7ea964bb1145e68c214f691a8a1601b68c
Duc Ho <duchx@global-hi.tech>
2021-06-07 16:55:15 +0900

    Commit source backend

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2d1d3be31545a53aefa3a091d352913'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d01d1de-a9ac-4861-a923-5ee78e633349'
+};
